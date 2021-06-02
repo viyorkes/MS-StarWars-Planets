51d63e9a5c18f2fb42da68d06541e2a29cfa587d
victor <victorsleiman25@gmail.com>
2021-06-02 12:07:07 -0300

    Create Project

---
+++ src/main/java/com/planets/starwars/StarWarsPlanetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.planets.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsPlanetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsPlanetsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planets/starwars/StarWarsPlanetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planets.starwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsPlanetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
